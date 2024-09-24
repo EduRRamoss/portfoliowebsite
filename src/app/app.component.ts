e7691729ab2d77ca7f1ddf085b5b042a18679992
Eduardo <eduardorodriguesr@outlook.com>
2024-09-24 19:55:56 -0300

    update: pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +15,12 @@ export class AppComponent {
   title = 'eduardorrportfolio';
 
   showFiller = false;
+
+  tiles: Tile[] = [
+    //{ cols: 4, rows: 1, color: 'red' },
+    { cols: 3, rows: 1, color: 'lightblue' },
+    { cols: 1, rows: 2, color: 'lightgreen' },
+    { cols: 1, rows: 1, color: 'lightpink' },
+    { cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {MatCardModule} from '@angular/material/card';
     AppRoutingModule,
     MatSidenavModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [
     provideAnimationsAsync()
